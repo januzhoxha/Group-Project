5313eb5e612279b2e853c2518617a53a2b086cd2
rinionjanuzaj <januzajmonda77@gmail.com>
2026-01-03 16:03:47 +0100

    funksioni nota mesatare

--- Rinion/studenti.cpp
+++ Rinion/studenti.cpp
@@ -10,7 +10,7 @@ string Id;
 int Notat[5];
 void lexo();
 void shtyp();
-void notaMes();
+double notaMes();
 
 };
 
@@ -27,21 +27,39 @@ void Studenti::lexo(){
         cin>> Notat[i];
         
     }
-     }
+     };
 
 
 void Studenti::shtyp(){
     cout<<"Emri: "<<Emri<<endl; 
 cout<<"Mbiemri: "<<Mbiemri<<endl;
 cout<<"Id: "<<Id<<endl;
+};
+
+
+double Studenti::notaMes(){
+int shuma= 0;
+int count= 0;
+for (int i = 0 ; i < 5 ; i++){
+    if (Notat[i]>5 && Notat[i]<=10 ){
+        shuma += Notat[i];
+        count++;
+    }
+    else if (Notat[i] == 5)
+    {
+    cout<<"Nuk e keni kaluar lenden.";
+    }
+    else{
+        cout<<"Ju lutem vendoseni nje vler prej  5 deri ne 10";
+        continue;
+    }
+return shuma/count;
 }
-
-
+ }
 
 
 
 int main(){
-
 
 
 
