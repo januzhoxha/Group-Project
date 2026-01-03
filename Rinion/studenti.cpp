dd251df85a5158f5e8000497a4b3c184ecc28793
rinionjanuzaj <januzajmonda77@gmail.com>
2026-01-03 15:50:48 +0100

    funksioni lexo dhe shtyp

--- Rinion/studenti.cpp
+++ Rinion/studenti.cpp
@@ -2,14 +2,42 @@
 using namespace std;
 
 
-struct studenti
+struct Studenti
 {
 string Emri;
 string Mbiemri;
 string Id;
 int Notat[5];
+void lexo();
+void shtyp();
+void notaMes();
 
 };
+
+
+void Studenti::lexo(){
+    cout<<"Vendoseni Emri"<<endl;
+    cin >> Emri;
+    cout<<"Vendoseni Mbiemri"<<endl;
+    cin >> Mbiemri;
+    cout<<"Vendoseni Id"<<endl;
+    cin >> Id;
+    cout<<"Vendoseni Notat"<<endl;
+    for(int i = 0 ; i < 5;i++){
+        cin>> Notat[i];
+        
+    }
+     }
+
+
+void Studenti::shtyp(){
+    cout<<"Emri: "<<Emri<<endl; 
+cout<<"Mbiemri: "<<Mbiemri<<endl;
+cout<<"Id: "<<Id<<endl;
+}
+
+
+
 
 
 int main(){
